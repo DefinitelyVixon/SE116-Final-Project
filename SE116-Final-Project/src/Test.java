9f50453763033abf7987489dd022bf89bfd5be9c
Ywhire <yinan2001@hotmail.com>
2020-05-23 22:12:50 +0300

    bruh

--- SE116-Final-Project/src/Test.java
+++ SE116-Final-Project/src/Test.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -9,6 +12,36 @@ public class Test {
         System.out.println();
         System.out.println();
         System.out.println();
+    }
+    static Scanner scan = new Scanner(System.in);
+
+    private static int menu() {
+        while (true) {
+            System.out.println("<<-------------------------------------------------------->>");
+            System.out.println("|               OASIS VOL 2                               |");
+            System.out.println("|        Izmir University of Economics                    |");
+            System.out.println("|---------------------------------------------------------|");
+            System.out.println("|-------       1.Lecturer     -------    2.Student    ----|");
+            System.out.println("<<________________________________________________________>>");
+            System.out.print("|------Select one :");
+            try {
+                int selection = scan.nextInt();
+                space();
+                if(selection==2||selection==1){
+                    return selection;
+                }
+                else {
+                    System.out.println("Yazık kafana !");
+                }
+            } catch (InputMismatchException e) {
+                scan.nextLine();
+                System.out.println("Invalid input please try integer ");
+                System.out.println();
+            }
+        }
+    }
+    private static void space() {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
 
 
