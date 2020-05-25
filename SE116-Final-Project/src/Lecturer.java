cd141d420229e57242393ddfcf1b7dc22105554d
Ywhire <yinan2001@hotmail.com>
2020-05-26 00:57:58 +0300

    Some changes

--- SE116-Final-Project/src/Lecturer.java
+++ SE116-Final-Project/src/Lecturer.java
@@ -87,34 +87,24 @@ public class Lecturer extends Academic implements Functional {
             // Select Section
             int sectionIndex = sc.nextInt() - 1;
 
-            staticMenu("GRADES");
+                staticMenu("GRADES");
+            System.out.println("  ╠═══════════════════════════════════════════════════════════╝");
 
-            int length = 31 + 11 * getLectures().get(lectureIndex).getSections().get(sectionIndex).getSectionStudents().get(0).getMyGrades().get(0).getCourseGrades().size();
+            System.out.print("  ╟──┤" + StringUtils.center("ID",11) + "├──┤" + StringUtils.center("NAME",11) +"│     " );
+                for (Grade g :  getLectures().get(lectureIndex).getSections().get(sectionIndex).getSectionStudents().get(0).getMyGrades().get(0).getCourseGrades()){
 
-            if (length < 61) {
-
-                System.out.println("  ╠═══════════════════════════════════════════════════════════╣");
-            }
-            else {
-
-                System.out.println("  ╠═══════════════════════════════════════════════════════════╩");
-            }
-
-            System.out.print("  ╟──┤" + StringUtils.center("ID", 11) + "├──┤" + StringUtils.center("NAME", 11) + "├");
-            for (Grade g : getLectures().get(lectureIndex).getSections().get(sectionIndex).getSectionStudents().get(0).getMyGrades().get(0).getCourseGrades()) {
-
-                System.out.print("──┤" + StringUtils.center(g.getGradeType(), 7) + "├");
-            }
+                    System.out.print("  │"+StringUtils.center(g.getGradeType(),7)+"│");
+                }
 
             System.out.println();
             // Print the Grades of that Section Students
             for (Student student : getLectures().get(lectureIndex).getSections().get(sectionIndex).getSectionStudents()) {
 
-                System.out.print(student.getName() + student.getID());
-                for (Grade g : student.getMyGrades().get(0).getCourseGrades()) {
+                System.out.print ("  ║  │" +student.getID() +" │  │"+ student.getName() + "|     ");
+                for (Grade g : student.getMyGrades().get(0).getCourseGrades()){
 
 
-                    System.out.print("──┤   " + g.getGradeValue() + "   ├");
+                    System.out.print("  │"+StringUtils.center(g.getGradeValue(),7)+"│");
                 }
                 System.out.println();
             }
@@ -149,11 +139,13 @@ public class Lecturer extends Academic implements Functional {
         while(true) {
 
             int i = 1;
+            System.out.println();//bl
             for(String s : localChanges){
 
                 System.out.println(i + "- " + s);
                 i++;
             }
+            System.out.println();
 
             System.out.println("Please Enter the Student ID. Enter -1 to Exit Edit Mode");
             String selection = sc.next();
